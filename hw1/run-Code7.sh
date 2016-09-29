893923217144f9d643a81ce2689a2585a33f9833
Chaitanya Ahuja <ahujachaitanya@gmail.com>
2016-09-29 00:14:17 -0400

    updated scripts

---
+++ hw1/run-Code7.sh
@@ -0,0 +1,4 @@
+python backprob.py --lr 0.01 --save_dir a1
+python backprob.py --lr 0.01 --save_dir a2
+python backprob.py --lr 0.01 --save_dir a3
+python backprob.py --lr 0.01 --save_dir a4
--- hw1/run.sh
+++ hw1/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ## 5 for first
+python backprob.py --lr 0.01 --save_dir a1
+python backprob.py --lr 0.01 --save_dir a2
+python backprob.py --lr 0.01 --save_dir a3
+python backprob.py --lr 0.01 --save_dir a4
 ##
 #d)
 python backprob.py --lr 0.1 --save_dir d
@@ -25,6 +29,12 @@ python backprob.py --lr 0.01 --mm 0.5 --graph 784 500 10 --save_dir e
 python backprob.py --lr 0.01 --mm 0.5 --graph 784 100 10 --prob 0.5 --save_dir f
 python backprob.py --lr 0.01 --mm 0.5 --graph 784 200 10 --prob 0.5 --save_dir f
 python backprob.py --lr 0.01 --mm 0.5 --graph 784 500 10 --prob 0.5 --save_dir f
+
+#g) best network
+python backprob.py --lr 0.01 --mm 0.5 --graph 784 200 10 --prob 0.5 --save_dir g --lam 0.1
+python backprob.py --lr 0.01 --mm 0.5 --graph 784 200 10 --prob 0.5 --save_dir g --lam 0.01
+python backprob.py --lr 0.01 --mm 0.5 --graph 784 200 10 --prob 0.5 --save_dir g 
+python backprob.py --lr 0.01 --mm 0.5 --graph 784 200 100 10 --prob 0.5 --save_dir g 
 
 #h) bigger network
 python backprob.py --lr 0.01 --mm 0.5 --graph 784 100 100 10 --prob 0.5 --save_dir h
